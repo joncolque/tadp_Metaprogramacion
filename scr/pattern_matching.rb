c0eaef3e678e5edab71e5ea16db5f79ee2fbc03d
JorgeLuis <lescanojorgeluis@gmail.com>
2016-04-26 20:39:23 -0300

    se sube el 1.a

--- scr/pattern_matching.rb
+++ scr/pattern_matching.rb
@@ -1,1 +1,39 @@
-#test
+class Matcher
+
+  attr_accessor :variables
+
+  def initialize
+    @variables=[]
+  end
+
+  def a_variable_name(nombre_variable, tipo_bindind)
+    nombre_variable = tipo_bindind
+    true
+  end
+
+  def val
+
+  end
+
+  def type
+
+  end
+end
+
+
+class Combinators
+
+  def and
+
+  end
+
+  def or
+
+  end
+
+  def not
+
+  end
+
+
+end
--- test/pattern_matching_spec.rb
+++ test/pattern_matching_spec.rb
@@ -1,9 +1,13 @@
 require 'rspec'
+require_relative '../scr/pattern_matching'
 
-describe 'My behaviour' do
+describe 'Pattern Mathing' do
 
-  it 'should do something' do
 
-    true.should == false
+
+  it 'test para probar bindear una variable' do
+    a='hola'
+    algo = Matcher.new
+    expect(algo.a_variable_name(a, 'anything')).to eq(true)
   end
 end
